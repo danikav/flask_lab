3f93cbdbaa5752148f47779005ff9d62536e56c0
Dani <danikav1989@gmail.com>
2021-03-10 21:06:26 +0000

    advanced extensions completed

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -3,11 +3,18 @@ from flask import render_template
 from app import app
 from app.models.online_shop import orders
 
+@app.route('/')
+def home():
+    return render_template(
+        'index.html',
+        title="Drinkz R Us"
+    )
+
 @app.route('/orders')
 def index():
     return render_template(
         'order.html',
-        title='Drinkz R Us',
+        title='Drinkz R Us - Orders',
         orders= orders
     )
 
@@ -15,7 +22,7 @@ def index():
 def show_by_id(index):
     return render_template(
         'find_order.html',
-        title='Drinkz R Us',
+        title='Drinkz R Us - View Order',
         index=index,
         orders=orders
     )
